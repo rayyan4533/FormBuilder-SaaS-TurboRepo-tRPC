03d7a876a3905731f116204b93d123cdd05ecc60
rayya <mohammedrayyan.ansari280@nmims.edu.in>
2026-07-25 15:00:10 +0530

    Updated project structure for my app

--- packages/database/models/user.ts
+++ packages/database/models/user.ts
@@ -1,25 +1,25 @@
-import {
-  pgTable,
-  uuid,
-  varchar,
-  timestamp,
-  boolean,
-  text,
-} from "drizzle-orm/pg-core";
+// import {
+//   pgTable,
+//   uuid,
+//   varchar,
+//   timestamp,
+//   boolean,
+//   text,
+// } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: uuid("id").primaryKey().defaultRandom(),
+// export const usersTable = pgTable("users", {
+//   id: uuid("id").primaryKey().defaultRandom(),
 
-  fullName: varchar("full_name", { length: 80 }).notNull(),
+//   fullName: varchar("full_name", { length: 80 }).notNull(),
 
-  email: varchar("email", { length: 255 }).notNull().unique(),
-  emailVerified: boolean("email_verified").default(false),
+//   email: varchar("email", { length: 255 }).notNull().unique(),
+//   emailVerified: boolean("email_verified").default(false),
 
-  profileImageUrl: text("profile_image_url"),
+//   profileImageUrl: text("profile_image_url"),
 
-  createdAt: timestamp("created_at").defaultNow(),
-  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
-});
+//   createdAt: timestamp("created_at").defaultNow(),
+//   updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+// });
 
-export type SelectUser = typeof usersTable.$inferSelect;
-export type InsertUser = typeof usersTable.$inferInsert;
+// export type SelectUser = typeof usersTable.$inferSelect;
+// export type InsertUser = typeof usersTable.$inferInsert;
