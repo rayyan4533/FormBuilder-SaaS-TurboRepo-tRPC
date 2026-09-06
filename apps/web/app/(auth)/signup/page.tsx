b37c1bc1ed4d14f78261f126de9d755dcd477046
Mohammed Rayyan Ansari <mohammedrayyan.ansari280@nmims.edu.in>
2026-09-06 17:16:49 +0530

    i am testing

--- apps/web/app/(auth)/signup/page.tsx
+++ apps/web/app/(auth)/signup/page.tsx
@@ -4,6 +4,7 @@ export default function SignupPage() {
     return (
         <main className="flex justify-center items-center bg-background p-4 min-h-screen">
             <SignUpForm />
+            <h2>Hi i am testing you raycode pr reviewer</h2>
         </main>
     );
 }
