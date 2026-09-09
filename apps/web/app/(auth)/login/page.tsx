779ec8b32c4dadb98a4502a2de9cd7c58725ba5f
Mohammed Rayyan Ansari <mohammedrayyan.ansari280@nmims.edu.in>
2026-09-09 19:15:12 +0530

    add testing messages to login and signup pages

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ export default function LoginPage() {
     return (
         <main className="flex justify-center items-center bg-background p-4 min-h-screen">
             <LoginForm />
+            <h2>Hi i am testing you raycode pr reviewer</h2>
         </main>
     );
 }
